5fde10e90efea685626167e1f1574082a122129a
a1740942002 <a1740942002@me.com>
2022-04-19 21:11:31 +0800

    Finish CollectNewspapaer

--- Assignment1/CollectNewspaperKarel.py
+++ Assignment1/CollectNewspaperKarel.py
@@ -14,8 +14,55 @@ from karel.stanfordkarel import *
 
 def main():
     """
-    TODO:
+    TODO: Karel need to go out of the box and pick the beeper, then go back to the inital position.
+    Pre-condition: Karel is at [3, 4] and the beeper is at [6, 3].
+    Post-condition: Karel is at [ 3,4 ] and the beeper is gone.
     """
+    go_out()
+    pick_beeper()
+    go_back()
+
+
+def go_out():
+    """
+    This function will make karel go out of the box.
+    """
+    turn_right()
+    move()
+    turn_left()
+    move()
+    move()
+    move()
+
+
+def go_back():
+    """
+    This function will make karel go back to its original position from the beeper position.
+    """
+    turn_around()
+    move()
+    move()
+    move()
+    turn_right()
+    move()
+    turn_right()
+
+
+def turn_right():
+    """
+    This function will turn karel to its left 3 times, which means it will end up turn to its original facing right.
+    """
+    for i in range(3):
+        turn_left()
+
+def turn_around():
+    """
+    This function will turn karel to its left 2 times, which means it will end up turn to its original facing back.
+    """
+    for i in range(2):
+        turn_left()
+
+
 
 
 # DO NOT EDIT CODE BELOW THIS LINE #
