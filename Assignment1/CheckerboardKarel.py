47ac8bb08c31335f64fd9007d522ec59308c6cf5
a1740942002 <a1740942002@me.com>
2022-04-21 22:47:11 +0800

    Add A1Q3 Comment

--- Assignment1/CheckerboardKarel.py
+++ Assignment1/CheckerboardKarel.py
@@ -14,30 +14,55 @@ from karel.stanfordkarel import *
 def main():
     """
     TODO:
+    Karel need to put beepers on the world, and each beeper should have 1 space next to others.
+    So Karel just have to put beeper every two step, then could fit its need.
+    Because Karel will eventually mpve and put beeper from South to North, so Karel when meet the edge, it must be facing North.
+
+    pre-condition: Karel will start at [1, 1], facing East.
+    post-condition: Karel will end at last line of the world, facing North.
     """
     put_beeper()
     while not facing_north():
         put_beeper_by_two_steps()
 
 def put_beeper_by_two_steps():
+    """
+    Because Our algorithm is "Karel should put beeper every two step", so we just need to focus on how to move 2 step without hitting the wall.
+
+    When hit the edge, safe_step will make Karel face North, so Karel should not put beeper when it's facing North.
+    """
     for i in range(2):
-        if front_is_clear():
-            move()
-        else:
-            if facing_east():
-                turn_left()
-                if front_is_clear():
-                    move()
-                    turn_left()
-            else:
-                if facing_west():
-                    turn_right()
-                    if front_is_clear():
-                        move()      
-                        turn_right()
+        safe_step()
     if not facing_north():
         put_beeper()
 
+
+def safe_step():
+    """
+    In normal situation, Karel will move 1 step, and if it hit the wall he will automatically turn to left or right ( depend on where it's facing, if it's facing East, it will turn left; if it's facing West, it will turn right, because it eventually want to move toward North ).
+
+    But if Karel hit the edge, it will face North and stand still without move any step.
+
+    pre-condition: Karel should face East or West, no matter where it is.
+    post-condition:
+        1. Karel might face West or East, and 1 step away from it's start posiiton.
+        2. Karel face North, and stand still from it's start position.
+    """
+    if front_is_clear():
+        move()
+    else:
+        # Because front is not clear, so let's turn!
+        if facing_east():
+            turn_left()
+            if front_is_clear():
+                move()
+                turn_left()
+        else:
+            if facing_west():
+                turn_right()
+                if front_is_clear():
+                    move()      
+                    turn_right()
 
 def turn_right():
     """
