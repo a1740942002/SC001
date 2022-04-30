9a182687c771da3ed02ae050b5d775f66ecdf337
a1740942002 <a1740942002@me.com>
2022-04-30 22:22:18 +0800

    Finish A2Q1

--- Assignment2/quadratic_solver.py
+++ Assignment2/quadratic_solver.py
@@ -1,6 +1,6 @@
 """
 File: quadratic_solver.py
-Name:
+Name: Brian Lai
 -----------------------
 This program should implement a console program
 that asks 3 inputs (a, b, and c)
@@ -16,47 +16,61 @@ import math
 
 
 def main():
-	"""
-	TODO:
-	"""
-	greeting()
+    """
+		TODO: Ask user to input 3 variables, and to display the root result.
+		"""
 
-	a = int(input('Enter a: '))
-	b = int(input('Enter b: '))
-	c = int(input('Enter c: '))
-	discriminantResult = getDiscriminant(a, b, c)
-	result = ''
+    # Greeting
+    print('stanCode Quadratic Solver!')
 
-	if discriminantResult > 0:
-		answer1 = getRoot(a,b,c, '+')
-		answer2 = getRoot(a,b,c, '-')
-		result = 'Two roots: ' + answer1  + ', ' + answer2
-	elif discriminantResult == 0:
-		answer = getRoot(a,b,c, '+')
-		result = 'One root: ' + answer
-	else:
-		result = 'No real roots'
+    a = int(input('Enter a: '))
+    b = int(input('Enter b: '))
+    c = int(input('Enter c: '))
+    discriminantResult = getDiscriminant(a, b, c)
+    result = ''
 
-	print(result)
+    if discriminantResult > 0:
+        answer1 = getRoot(a, b, c, '+')
+        answer2 = getRoot(a, b, c, '-')
+        result = 'Two roots: ' + str(answer1) + ', ' + str(answer2)
+    elif discriminantResult == 0:
+        answer = getRoot(a, b, c, '+')
+        result = 'One root: ' + str(answer)
+    else:
+        result = 'No real roots'
 
-
-def greeting():
-	print('stanCode Quadratic Solver!')
+    print(result)
 
 
 def getDiscriminant(a, b, c):
-	return b^2 - 4 * a * c
+    """
+		Returns a discriminant result ( int ).
+
+    :param a: int, x^2's variable.
+    :param b: int, x^1's variable.
+    :param c: int, x^0's variable.
+    """
+    return b * b - 4 * a * c
 
 
 def getRoot(a, b, c, operator):
-	result = -INFINITY
-	if operator == '+':
-		result = (-b + math.sqrt(getDiscriminant(a, b, c))) / 2 * a
-	elif operator == '-':
-		result = (-b - math.sqrt(getDiscriminant(a, b, c))) / 2 * a
-	return result
+    """
+		Returns a root result ( int ).
+
+		:param a: int, x^2's variable.
+		:param b: int, x^1's variable.
+		:param c: int, x^0's variable.
+		:param operator: str, should be '+' or '-'.
+		"""
+    result = -INFINITY
+    if operator == '+':
+        result = (-b + math.sqrt(getDiscriminant(a, b, c))) / 2 * a
+    elif operator == '-':
+        result = (-b - math.sqrt(getDiscriminant(a, b, c))) / 2 * a
+    return result
+
 
 ###### DO NOT EDIT CODE BELOW THIS LINE ######
 
 if __name__ == "__main__":
-	main()
+    main()
