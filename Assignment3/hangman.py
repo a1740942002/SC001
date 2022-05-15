88a3079e19b4c57529b042050dcd6c21241e00a3
a1740942002 <a1740942002@me.com>
2022-05-15 21:51:01 +0800

    Finished A3Q4

--- Assignment3/hangman.py
+++ Assignment3/hangman.py
@@ -18,12 +18,86 @@ import random
 # This constant controls the number of guess the player has.
 N_TURNS = 7
 
-
 def main():
     """
     TODO:
     """
-    pass
+
+    # Init Data
+    left_life = N_TURNS
+    answer = random_word()
+    left_answer = answer
+    guess = get_init_guess(len(answer))
+
+    while True:
+        # Lose Condition
+        if left_life <= 0:
+            print('You are completely hung : (')
+            break
+
+        # Win Condition
+        elif guess == answer:
+            print('You win!!')
+            break
+
+        # Normal Condition
+        print('The word looks like ' + guess)
+        print('You have ' + str(left_life) + ' wrong guesses left.')
+        raw_guess_char = input('Your guess: ')
+        guess_char = raw_guess_char.upper()
+        is_guess_correct = answer.find(guess_char) >= 0
+
+        if is_guess_correct:
+            print('You are correct!')
+            guess = get_updated_guess(guess, answer, guess_char)
+            left_answer = get_updated_left_answer(left_answer, guess_char)
+        else:
+            print("There is no " + guess_char + "'s in the word.")
+            left_life -= 1
+
+    print('The word was: ' + answer)
+
+
+def get_updated_left_answer(left_answer, guess_char):
+    """
+    :param left_anser: string, initial left answer.
+    :param guess_char: string, user's guess character in this round.
+    :return : string, consist of left chars in answer which user haven't guessed.
+    """
+    updated_left_answer = ''
+    for i in range(len(left_answer)):
+        if left_answer[i] != guess_char:
+            updated_left_answer += left_answer[i]
+
+    return updated_left_answer
+
+
+def get_updated_guess(guess, answer, guess_char):
+    """
+    :param guess: string, initial guess string.
+    :param answer: string, this round's correct answer.
+    :param guess_char: string, user's guess character in this round.
+    :return : string, consist of left chars in answer which user have guessed.
+    """
+    updated_guess = ''
+    for i in range(len(guess)):
+        if answer[i] == guess_char:
+            updated_guess += guess_char
+        else:
+            updated_guess += guess[i]
+
+    return updated_guess
+
+
+def get_init_guess(number):
+    """
+    :param number: int, how many dash will be created.
+    :return : a string consist of '-' which length is equal to input number.
+    """
+    guess = ''
+    for i in range(number):
+        guess += '-'
+    return guess
 
 
 def random_word():
